75c3f00222186a402c07f2f1ea257839da4a95a2
Alexey Cheltigmashev <rovan3014@mail.ru>
2019-04-25 00:04:08 +0700

    Фикс добавления контактов

--- test_add_contact.py
+++ test_add_contact.py
@@ -7,8 +7,11 @@ from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
 father = Contcat("Alex", "Rovan", "CFT", "Novosibirsk", "123321321", "12331223", "413243243", "weqwwe@sffse.ri")
+empty = Contcat("", "", "", "", "", "", "", "")
+
 
 class TestAddContact(unittest.TestCase):
+
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
@@ -17,7 +20,7 @@ class TestAddContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, "admin", "secret")
-        self.create_contcat(wd,father)
+        self.create_contcat(wd, father)
         self.return_home_page(wd)
         self.logout(wd)
 
@@ -25,7 +28,7 @@ class TestAddContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, "admin", "secret")
-        self.create_contcat(wd, father)
+        self.create_contcat(wd, empty)
         self.return_home_page(wd)
         self.logout(wd)
 
@@ -35,32 +38,32 @@ class TestAddContact(unittest.TestCase):
     def return_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
 
-    def create_contcat(self, wd, firstname, lastname, company, address, home_phone, mobile_phone, work_phone, email):
+    def create_contcat(self,wd, contact):
         # add contact
         wd.find_element_by_link_text("add new").click()
         # fill contact
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(home_phone)
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobile_phone)
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(work_phone)
+        wd.find_element_by_name("work").send_keys(contact.work_phone)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email)
+        wd.find_element_by_name("email").send_keys(contact.email)
         # create
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
@@ -89,7 +92,6 @@ class TestAddContact(unittest.TestCase):
             return False
         return True
 
-    
     def tearDown(self):
         self.wd.quit()
 
