415a8d5bc4b6136aa8b28e6294ba23bbd6d8bda5
Alexey Cheltigmashev <rovan3014@mail.ru>
2019-06-26 23:18:41 +0700

    отчет allure

--- tests/group/test_delete_group.py
+++ tests/group/test_delete_group.py
@@ -2,16 +2,20 @@
 import random
 from data.group import empty_group
 from model.group import Group
-
+import allure
 
 def test_delete_group_by_index(app, db, check_ui):
-    if len(db.get_group_list()) == 0:
-        app.group.create(empty_group)
-    old_groups = db.get_group_list()
-    group = random.choice(old_groups)
-    app.group.delete_group_by_id(group.id)
-    new_groups = db.get_group_list()
-    old_groups.remove(group)
-    assert old_groups == new_groups
-    if check_ui:
-        assert sorted(new_groups, key = Group.id_or_max) == sorted(app.group.get_groups_list(), key= Group.id_or_max)
+    with allure.step('Add group, if no groups now'):
+        if len(db.get_group_list()) == 0:
+            app.group.create(empty_group)
+    with allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with allure.step('Delete random Group'):
+        group = random.choice(old_groups)
+        app.group.delete_group_by_id(group.id)
+    with allure.step('Group removed'):
+        new_groups = db.get_group_list()
+        old_groups.remove(group)
+        assert old_groups == new_groups
+        if check_ui:
+            assert sorted(new_groups, key = Group.id_or_max) == sorted(app.group.get_groups_list(), key= Group.id_or_max)
--- tests/group/test_edit_group.py
+++ tests/group/test_edit_group.py
@@ -1,18 +1,22 @@
 from model.group import Group
 import random
-
+import allure
 
 def test_edit_group_by_index(app,db,json_groups,check_ui):
-    group = json_groups
-    if app.group.count() == 0:
-        app.group.create(group)
-    old_groups = db.get_group_list()
-    group_edit = random.choice(old_groups)
-    group.id = group_edit.id
-    app.group.edit_group_by_id(group,group_edit.id)
-    new_groups = db.get_group_list()
-    old_groups.remove(group_edit)
-    old_groups.append(group)
-    assert sorted(new_groups, key=Group.id_or_max) == sorted(old_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(new_groups, key = Group.id_or_max) == sorted(app.group.get_groups_list(), key= Group.id_or_max)
+    with allure.step('Add group, if no groups now'):
+        group = json_groups
+        if app.group.count() == 0:
+            app.group.create(group)
+    with allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with allure.step('Modify random Group'):
+        group_edit = random.choice(old_groups)
+        group.id = group_edit.id
+        app.group.edit_group_by_id(group,group_edit.id)
+    with allure.step('Group modified'):
+        new_groups = db.get_group_list()
+        old_groups.remove(group_edit)
+        old_groups.append(group)
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(old_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_groups, key = Group.id_or_max) == sorted(app.group.get_groups_list(), key= Group.id_or_max)
